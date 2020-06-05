27e23080db37ae75c980c1f8bd8bb1f3b8ab9a62
tanviredu <ornobtanvir.git@gmail.com>
2020-06-05 13:36:55 +0600

    added custom model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+##admin.site.register(Post)
+
+
+## customize the models and its display
+
+## this is a static method that force you to login
+## before accessing the model
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
+
+    ## how you ncan filet or search the post in the adminpanel
+    list_filter = ('status','created','publish','author')
+    search_field = ('title','body')
+    ## you have to give the comma
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields =('author',) ## you can search author when posting data
+    date_hierarchy = 'publish'
+    ordering = ('status','publish')
